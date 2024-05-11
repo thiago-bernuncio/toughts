b2c6d50d08dc57ba8935ac60537f7249f972a11a
Thiago Bernuncio <thiago.bernuncio@gmail.com>
2024-05-11 19:25:56 -0300

    Criação e configuração dos models Toughts e User

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express()
 
 const conn = require('./db/conn')
 
+// Models
+const Tought = require('./models/Tought')
+const User = require('./models/User')
+
 // template engine
 app.engine('handlebars', exphbs.engine())
 app.set('view', 'handlebars')
@@ -56,6 +60,7 @@ app.use((req, res, next) => {
 })
 
 conn
+    // .sync({ force: true })
     .sync()
     .then(() => {
         app.listen(3000)
